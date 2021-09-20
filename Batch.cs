9cc949596b32f04d068a733d0e2ec51c869434c3
Michael Kaminsky <38765120+KAMINM@users.noreply.github.com>
2021-09-20 13:42:36 -0400

    Add files via upload

---
+++ Batch.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatchParallel
+{
+    public static class Batch
+    {
+        public static Task[] Parallel<T>(this IEnumerable<T> collection, Action<IEnumerable<T>> action, bool singleThread = false)
+        {
+            int processors = singleThread ? 1 : Environment.ProcessorCount;
+            int n = collection.Count();
+            int nPerProc = n / processors;
+            Task[] tasks = new Task[processors + 1];
+
+            processors.ForEach(p => tasks[p] = Task.Run(() => action(collection.Skip(p * nPerProc).Take(nPerProc))));
+
+            int remainder = n - nPerProc * processors;
+            var lastTask = Task.Run(() => action(collection.Skip(nPerProc * processors).Take(remainder)));
+            tasks[processors] = lastTask;
+
+            return tasks;
+        }
+    }
+
+    public static class ExtensionMethods
+    {
+        // Process a subset of the collection on separate threads.
+        public static Task[] BatchParallel<T>(this IEnumerable<T> collection, Action<IEnumerable<T>> action, bool singleThread = false)
+        {
+            int processors = singleThread ? 1 : Environment.ProcessorCount;
+            int n = collection.Count();
+            int nPerProc = n / processors;
+            Task[] tasks = new Task[processors + 1];
+
+            processors.ForEach(p => tasks[p] = Task.Run(() => action(collection.Skip(p * nPerProc).Take(nPerProc))));
+
+            int remainder = n - nPerProc * processors;
+            var lastTask = Task.Run(() => action(collection.Skip(nPerProc * processors).Take(remainder)));
+            tasks[processors] = lastTask;
+
+            return tasks;
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> collection, Action<T> action)
+        {
+            foreach (var item in collection)
+            {
+                action(item);
+            }
+        }
+
+        public static void ForEach(this int n, Action<int> action)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                action(i);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿//https://www.codeproject.com/Articles/5289661/Batch-Parallel
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BatchParallel
+{
+    class Program
+    {
+        static ConcurrentDictionary<int, int> threadIdCounts;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Parallel.ForeEach example:");
+            threadIdCounts = new ConcurrentDictionary<int, int>();
+            var plr = Parallel.ForEach(Enumerable.Range(0, 1000), DoSomething);
+            threadIdCounts.ForEach(kvp => Console.WriteLine($"TID: {kvp.Key}, Count = {kvp.Value}"));
+
+            Console.WriteLine("\r\nBatchParallel example:");
+            threadIdCounts = new ConcurrentDictionary<int, int>();
+            var tasks = Enumerable.Range(0, 1000).BatchParallel(batch => DoSomething(batch));
+            Task.WaitAll(tasks);
+            threadIdCounts.ForEach(kvp => Console.WriteLine($"TID: {kvp.Key}, Count = {kvp.Value}"));
+
+            Console.WriteLine("\r\nBatchParallel with remainder example:");
+            threadIdCounts = new ConcurrentDictionary<int, int>();
+            tasks = Enumerable.Range(0, 1003).BatchParallel(batch => DoSomething(batch));
+            Task.WaitAll(tasks);
+            threadIdCounts.ForEach(kvp => Console.WriteLine($"TID: {kvp.Key}, Count = {kvp.Value}"));
+
+            Console.WriteLine("\r\nBatch.Parallel with remainder example:");
+            threadIdCounts = new ConcurrentDictionary<int, int>();
+            tasks = Batch.Parallel(Enumerable.Range(0, 1003), batch => DoSomething(batch));
+            Task.WaitAll(tasks);
+            threadIdCounts.ForEach(kvp => Console.WriteLine($"TID: {kvp.Key}, Count = {kvp.Value}"));
+        }
+
+        static void DoSomething(int n)
+        {
+            DoWork();
+        }
+
+        static void DoSomething<T>(IEnumerable<T> batch)
+        {
+            // Do setup stuff
+
+            // The process the batch.
+            batch.ForEach(n => DoWork());
+        }
+
+        static void DoWork()
+        {
+            int tid = Thread.CurrentThread.ManagedThreadId;
+
+            if (!threadIdCounts.TryGetValue(tid, out int count))
+            {
+                threadIdCounts[tid] = 0;
+            }
+
+            threadIdCounts[tid] = count + 1;
+            Thread.Sleep(1);
+        }
+    }
+}
